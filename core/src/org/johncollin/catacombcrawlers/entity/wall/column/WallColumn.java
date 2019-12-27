869edfae21e393b3c41275f088fcaea7abf9b5d5
JohnCollin <collin.g.johnson@gmail.com>
2019-12-27 00:41:58 -0500

    Add Javadoc for WallColumn class

--- core/src/org/johncollin/catacombcrawlers/entity/wall/column/WallColumn.java
+++ core/src/org/johncollin/catacombcrawlers/entity/wall/column/WallColumn.java
@@ -27,13 +27,37 @@ package org.johncollin.catacombcrawlers.entity.wall.column;
 import org.johncollin.catacombcrawlers.entity.Entity;
 import org.johncollin.catacombcrawlers.entity.StaticEntity;
 
+/**
+ * WallColumn base class
+ *
+ * @see org.johncollin.catacombcrawlers.entity.Entity
+ * @see org.johncollin.catacombcrawlers.entity.StaticEntity
+ *
+ * @since rev-004-1.0.0-SNAPSHOT
+ */
 public class WallColumn extends StaticEntity {
 	
+	/**
+	 * WallColumn basic constructor
+	 *
+	 * @param tx The coordinate of the x plane in a unit of standard game tiles.
+	 * @param ty The coordinate of the y plane in a unit of standard game tiles.
+	 *
+	 * @since rev-004-1.0.0-SNAPSHOT
+	 */
 	public WallColumn(int tx, int ty) {
 		super(tx * Entity.TILED_RES, ty * Entity.TILED_RES, Entity.TILED_RES, Entity.TILED_RES * 3,
 			80, 80, 16, 48);
 	}
 	
+	/**
+	 * WallColumn basic constructor with boolean hasWall flag
+	 *
+	 * @param tx The coordinate of the x plane in a unit of standard game tiles.
+	 * @param ty The coordinate of the y plane in a unit of standard game tiles.
+	 *
+	 * @since rev-004-1.0.0-SNAPSHOT
+	 */
 	public WallColumn(int tx, int ty, boolean hasWall) {
 		super(tx * Entity.TILED_RES, ty * Entity.TILED_RES, Entity.TILED_RES, Entity.TILED_RES * 3,
 			hasWall ? 96 : 80, 80, 16, 48);
